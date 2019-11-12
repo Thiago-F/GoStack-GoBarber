9364f220ae3ea16d19bae89c3bbb5d565f53614e
Thiago-F <thiago_fialho150@hotmail.com>
2019-11-12 08:16:52 -0300

    user store

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,18 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controller/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Thiago Fialho',
-        email: 'thiago@gmail.com',
-        password_hash: '123456',
-        provider: false,
-    });
-
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
