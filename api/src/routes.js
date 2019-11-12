412035c7fbb49b5d70abf5b336d8b9204f0c3840
Thiago-F <thiago_fialho150@hotmail.com>
2019-11-12 16:27:26 -0300

    add multer

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,13 +1,20 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
 routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+    return res.json({ ok: true });
+});
 
 export default routes;
