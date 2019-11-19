c38009d06f262996771cf38e5b1fca4c583a5bdb
Thiago-F <thiago_fialho150@hotmail.com>
2019-11-19 20:49:16 -0300

    armazenando notificações no mongo

--- api/src/app/controllers/AppointmentController.js
+++ api/src/app/controllers/AppointmentController.js
@@ -1,7 +1,11 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
     async index(req, res) {
@@ -58,10 +62,56 @@ class AppointmentController {
             });
         }
 
+        /**
+         * Check for past dates
+         */
+
+        const hourStart = startOfHour(parseISO(date));
+
+        if (isBefore(hourStart, new Date())) {
+            return res
+                .status(400)
+                .json({ error: 'Past dates are not permitted' });
+        }
+
+        /**
+         * Check availability
+         */
+
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if (checkAvailability) {
+            return res
+                .status(400)
+                .json({ error: 'Appointment date is not available' });
+        }
+
+        /**
+         * Notify appointment to provider
+         */
+
+        const user = await User.findByPk(req.userId);
+        const formattedDate = format(
+            hourStart,
+            "'dia' dd 'de' MMMM', ás' H:mm'h'",
+            { locale: pt }
+        );
+
+        await Notification.create({
+            content: `Novo agendamento para ${user.name} para ${formattedDate}`,
+            user: provider_id,
+        });
+
         const appointment = await Appointment.create({
             user_id: req.userId,
             provider_id,
-            date,
+            date: hourStart,
         });
 
         return res.json(appointment);
