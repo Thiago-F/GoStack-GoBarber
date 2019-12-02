974103e2c673cb45f20961ed2be255644b1575dc
Thiago-F <thiago_fialho150@hotmail.com>
2019-12-02 16:55:49 -0300

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
@@ -8,12 +9,16 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import store from './store';
+
 function App() {
     return (
-        <Router history={history}>
-            <Routes />
-            <GlobalStyle />
-        </Router>
+        <Provider store={store}>
+            <Router history={history}>
+                <Routes />
+                <GlobalStyle />
+            </Router>
+        </Provider>
     );
 }
 
