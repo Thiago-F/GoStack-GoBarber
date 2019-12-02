b577d4e3d14fc7c38358022d5cf9e7191346b29d
Thiago-F <thiago_fialho150@hotmail.com>
2019-12-02 14:40:40 -0300

    add eslint

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      {/* algo aqui */}
-    </div>
-  );
+  return <div className="App">{/* algo aqui */}</div>;
 }
 
 export default App;
