c46ded1b859685d75f1660b8b1cc909800f6e024
Thiago-F <thiago_fialho150@hotmail.com>
2019-12-03 16:05:10 -0300

    add persist

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import GlobalStyle from './styles/global';
 
@@ -9,15 +10,17 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
-import store from './store';
+import { store, persistor } from './store';
 
 function App() {
     return (
         <Provider store={store}>
-            <Router history={history}>
-                <Routes />
-                <GlobalStyle />
-            </Router>
+            <PersistGate persistor={persistor}>
+                <Router history={history}>
+                    <Routes />
+                    <GlobalStyle />
+                </Router>
+            </PersistGate>
         </Provider>
     );
 }
